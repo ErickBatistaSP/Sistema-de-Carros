4cd53475d2fd46555667626ec41a54bd1b2af714
Erick Batista <erickbatista748@gmail.com>
2026-05-09 23:58:07 -0300

    consertando bugs de carro sem ano de fabricacao

--- cars/forms.py
+++ cars/forms.py
@@ -15,6 +15,18 @@ class CarModelForm(forms.ModelForm):
   
   def clean_factory_year(self):
     factory_year = self.cleaned_data.get('factory_year')
-    if factory_year < 1975:
-      self.add_error('factory_year', 'Carro muito velho, vou adicionar isso nao (-1975)')
+    if factory_year is None:
+      self.add_error('factory_year', 'Ano de fabricaçao é obrigatorio')
+    elif factory_year < 1940:
+      self.add_error('factory_year', 'Carro muito velho, vou adicionar isso nao (-1940)')
+
     return factory_year
+  
+  def clean_model_year(self):
+    model_year = self.cleaned_data.get('model_year')
+    if model_year is None:
+      self.add_error('model_year', 'Ano do Modelo do carro é obrigatorio')
+    elif model_year < 1940:
+      self.add_error('model_year', 'Carro muito velho, vou adicionar isso nao (-1940)')
+
+    return model_year
